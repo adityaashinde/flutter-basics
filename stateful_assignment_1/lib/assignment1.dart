d4babe58e4bb3b88f95ea44f06ff9a4a0b638490
adityaashinde <adityashinde4580@gmail.com>
2024-01-23 14:29:56 +0530

    stateful-assignment-01 : done

--- stateful_assignment_1/lib/assignment1.dart
+++ stateful_assignment_1/lib/assignment1.dart
@@ -1,1 +1,64 @@
+import 'package:flutter/material.dart';
 
+class Assignment1 extends StatefulWidget {
+  const Assignment1({super.key});
+
+  @override
+  State<Assignment1> createState() => _Assignment1State();
+}
+
+class _Assignment1State extends State<Assignment1> {
+  int? _count = 0;
+
+  void _printTableValue() {
+    setState(() {
+      _count = _count! + 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Table of 2",
+          style: TextStyle(
+            color: Colors.purple,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Click button to print Table values :)",
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "$_count",
+              style: const TextStyle(color: Colors.purple, fontSize: 18),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _printTableValue,
+              child: const Text(
+                "Print",
+                style: TextStyle(color: Colors.purple, fontSize: 14),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
