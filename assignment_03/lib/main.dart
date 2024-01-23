ae4cbfb8cbd92bd3ecd837c915adc2768b645710
adityaashinde <adityashinde4580@gmail.com>
2024-01-23 12:09:35 +0530

    Assignment 03 : done

---
+++ assignment_03/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:assignment_03/assignment3.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Assignment3(),
+    );
+  }
+}
