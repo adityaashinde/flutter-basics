34dd3509253f153d7bf7d0d68e8f64231e6e509e
adityaashinde <adityashinde4580@gmail.com>
2024-01-24 09:48:32 +0530

    Update assignment2.dart

--- stateful_assignment_2/lib/assignment2.dart
+++ stateful_assignment_2/lib/assignment2.dart
@@ -19,6 +19,72 @@ class _Assignment2State extends State<Assignment2> {
           title: const Text("Color Box"),
           backgroundColor: Colors.blue,
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Box 1
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: box1Color ? Colors.red : Colors.black,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    // Box 1 Button
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          box1Color = true;
+                        });
+                      },
+                      child: const Text("Color Box 1"),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+
+                // Box 2
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: box2Color ? Colors.amber : Colors.black,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    // Box 2 Button
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            box2Color = true;
+                          },
+                        );
+                      },
+                      child: const Text("Color Box 2"),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
