5a27c04f1869231b067bf7e9bc0ea51391f58a98
adityaashinde <adityashinde4580@gmail.com>
2024-06-01 23:09:18 +0530

    Update main.dart

--- navigator_demo/lib/main.dart
+++ navigator_demo/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navigator_demo/screens/screen1.dart';
+import 'package:navigator_demo/screens/screen2.dart';
+import 'package:navigator_demo/screens/screen3.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +13,76 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Navigator Demo App!'),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigator Demo"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /// Button 1
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const Screen1();
+                    },
+                  ),
+                );
+              },
+              child: const Text("Screen 1"),
+            ),
+
+            const SizedBox(height: 25),
+
+            /// Button 2
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const Screen2();
+                    },
+                  ),
+                );
+              },
+              child: const Text("Screen 2"),
+            ),
+
+            const SizedBox(height: 25),
+
+            /// Button 3
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const Screen3();
+                    },
+                  ),
+                );
+              },
+              child: const Text("Screen 3"),
+            ),
+          ],
         ),
       ),
     );
